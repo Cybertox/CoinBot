da740b616d3c97d222b86fb58e9b24854b50b33e
Cybertox <oxetan@gmail.com>
2019-11-04 23:06:07 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 # webhook
 #URL = os.environ.get('URL')
 URL = "https://coin-bot1/herokuapp.com/"
-PORT = int(os.environ.get('PORT', '5000'))
+PORT = int(os.environ.get('PORT', '8443'))
 TOKEN = '754265584:AAHnnOT40LIcGsFNCM9UpTdjxPoXD4RWSoY'
 # telegram
 #TOKEN = os.environ.get('TOKEN')
