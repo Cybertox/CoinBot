64217398f107f858f81ebb240539c196fccaf64f
Cybertox <oxetan@gmail.com>
2019-11-04 22:56:05 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 # webhook
 URL = os.environ.get('URL')
 PORT = int(os.environ.get('PORT', '5000'))
-
+TOKEN = '754265584:AAHnnOT40LIcGsFNCM9UpTdjxPoXD4RWSoY'
 # telegram
-TOKEN = os.environ.get('TOKEN')
+#TOKEN = os.environ.get('TOKEN')
