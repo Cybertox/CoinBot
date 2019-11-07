249f25f3983d0c64301d353f0862ce5a81a9cc44
Cybertox <oxetan@gmail.com>
2019-11-08 00:40:44 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ URL = "https://coin-bot1/herokuapp.com/"
 #URL = os.environ.get('URL')
 #URL = os.environ.get('https://coin-bot1/herokuapp.com/')
 PORT = int(os.environ.get('PORT', '8443'))
-TOKEN = "754265584:AAHnnOT40LIcGsFNCM9UpTdjxPoXD4RWSoY"
+#TOKEN = "754265584:AAHnnOT40LIcGsFNCM9UpTdjxPoXD4RWSoY"
 # telegram
-#TOKEN = os.environ.get('754265584:AAHnnOT40LIcGsFNCM9UpTdjxPoXD4RWSoY')
+TOKEN = os.environ.get('TOKEN')
